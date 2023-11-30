eb613b03d49709c1921064414b7a3ab51a75194f
PinaMC <PinaMC@users.noreply.github.com>
2023-11-30 00:28:33 -0300

    cambio del 26
    varias implementaciones y validaciones de carga

--- ProyectoU2.cpp
+++ ProyectoU2.cpp
@@ -2,13 +2,18 @@
 #include <fstream>
 #include <queue>
 #include <vector>
+#include <sstream>
+#include <limits>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-//struct Ciudades (City,ConnectedCity)
+//formato del struct Ciudades (City,ConnectedCity)
 struct CIUDADES{
 
     string nombre;
-    vector <string> CiudadesConectadas;
+    vector <string> ciudadesConectadas;
        
 };
 //struct guardianes (Name,PowerLevel,Master,City)
@@ -22,70 +27,198 @@ struct GUARDIANES{
 
 };
 
+// Función para leer ciudades desde un archivo
+vector<CIUDADES> leerCiudades(const string &ciudadesArchivo)
+{
+    ifstream ciudadesFile(ciudadesArchivo);
+
+    if (!ciudadesFile.is_open())
+    {
+        cerr << "***ERROR EN EL ARCHIVO DE CIUDADES***" << endl;
+        exit(1);
+    }
+
+    string lineaCiudad;
+    vector<CIUDADES> ciudades;
+
+    while (getline(ciudadesFile, lineaCiudad))
+    {
+        stringstream ss(lineaCiudad);
+        string ciudad, conectada;
+
+        getline(ss, ciudad, ',');
+        CIUDADES nuevaCiudad;
+        nuevaCiudad.nombre = ciudad;
+
+        while (getline(ss, conectada, ','))
+        {
+            nuevaCiudad.ciudadesConectadas.push_back(conectada);
+        }
+
+        ciudades.push_back(nuevaCiudad);
+    }
+
+    ciudadesFile.close();
+    cout << "Lectura de ciudades hecha" << endl;
+    return ciudades;
+}
+// Función para construir un grafo no dirigido a partir de la información de ciudades
+unordered_map<string, vector<string>> construirGrafo(const vector<CIUDADES> &ciudades)
+{
+    unordered_map<string, vector<string>> grafo;
+
+    for (const auto &ciudad : ciudades)
+    {
+        // Añadir la ciudad al grafo si no está presente
+        if (grafo.find(ciudad.nombre) == grafo.end())
+        {
+            grafo[ciudad.nombre] = vector<string>();
+        }
+
+        // Añadir las ciudades conectadas
+        for (const auto &conectada : ciudad.ciudadesConectadas)
+        {
+            grafo[ciudad.nombre].push_back(conectada);
+
+            // Asegurarse de que la ciudad conectada también tenga una entrada en el grafo
+            if (grafo.find(conectada) == grafo.end())
+            {
+                grafo[conectada] = vector<string>();
+            }
+
+            // Agregar la ciudad actual a las ciudades conectadas (grafo no dirigido)
+            grafo[conectada].push_back(ciudad.nombre);
+        }
+    }
+
+    return grafo;
+}
+
+
+// Función para leer guardianes desde el archivo txt
+vector<GUARDIANES> leerGuardianes(const string& guardianFile) {
+    ifstream guardian(guardianFile);
+
+    if (!guardian.is_open()) {
+        cerr << "***ERROR EN EL ARCHIVO DE GUARDIANES***" << endl;
+        exit(1);
+    }
+
+    string lineaGuardian;
+    vector<GUARDIANES> guardianes;
+
+    //contador de la validacion
+    int contGuardianes100 = 0;  // Contador de guardianes con poder 100
+    int contGuardianesReino = 0; // Contador de guardianes con poder entre 90 y 99
+    //Text Formato de ejemplo [ City,ConnectedCity ]
+    while (getline(guardian, lineaGuardian)) {
+        stringstream ss(lineaGuardian);
+        string nombre, maestro, ciudad, poderStr;
+
+        getline(ss, nombre, ',');
+        getline(ss, poderStr, ',');
+        getline(ss, maestro, ',');
+        getline(ss, ciudad, ',');
+
+        GUARDIANES nuevoGuardian;
+        nuevoGuardian.nombre = nombre;
+        nuevoGuardian.poder = stoi(poderStr);
+        nuevoGuardian.maestro = maestro;
+        nuevoGuardian.ciudad = ciudad;
+
+        guardianes.push_back(nuevoGuardian);
+        //validar que solo haya un guardian con 100 y maximo 3 de entre 90 y 99
+        if (nuevoGuardian.poder == 100) {
+            contGuardianes100++;
+        } else if (nuevoGuardian.poder >= 90 && nuevoGuardian.poder <= 99) {
+            contGuardianesReino++;
+        }
+    }
+
+    guardian.close();
+
+    if (contGuardianes100 != 1 || (contGuardianesReino < 1 || contGuardianesReino > 3)) {
+        cout << "***ERROR*** En guardianes existe un error con el poder de los guardianes" << endl;
+        cerr << "Intenta revisar las reglas para el texto de guardianes" <<endl;
+        exit(1);// Terminar el programa con código de error
+    }
+    cout << "Lectura de guardianes hecha" << endl;
+    return guardianes;
+}
+// Función para mostrar todos los guardianes
+void mostrarGuardianes(const vector<GUARDIANES>& guardianes) {
+    cout << "Lista de todos los guardianes:" << endl;
+    for (const auto& guardian : guardianes) {
+        cout << "Nombre: " << guardian.nombre << ", Poder: " << guardian.poder << ", Maestro: " << guardian.maestro << ", Ciudad: " << guardian.ciudad << endl;
+    }
+}
+
+void reglas(){
+    cout << "***REGLAS**" << endl;
+    cout << "1- Solo puedes crear caminos al inicio del programa" << endl;
+    cout << "2- El programa solo funcionara si haces la correcta carga de archivos de texto" << endl;
+    cout << "3- Puedes elegir un guardian para jugar" << endl;
+    cout << "4- Si tienes mas puntos que el guardian al que enfrentas ganaras puntos y el rival perdera puntos" << endl;
+    cout << "5-" << endl;
+    cout << "6- " << endl;
+    cout << "7- " << endl;
+}
 
 
 int main(){
 
-    /*leer archivo contiene los datos de las capitales ( saco nombre de las capitales y caminos )
-    leer archivo saco {vertice} y saco el punto "A(V1,V2)"
-    El formato de lectura es de: string, string, int, string = nombre guardian, origen, "poder 1-99", "maestro";
-        //como leer archivos -> https://parzibyte.me/blog/2020/09/11/leer-archivo-texto-cpp/
-    En caso de usar matriz uso  
-    1- [V, indice]-> N°Vertices
-    2- los vertcies para la matriz quedan asi
-    Vertice-> indice, A->0, B->1, C->2
-    hacer un grafo para la forma del mapa
-    Un guardian puede ser aprendiz y maestro a la vez
-    Al vencer un a un maestro es de 5 pts y con un aprendiz 3
-    CIUDADES:
-    -Las ciudades se deben leer antes que los guardianes
-    GUARDIANES:
-    -Solo un guardian puede tener 100 puntos
-    -Solo 3 guardianes entre 90 y 99 puntos
-    -Deben vivir en una ciudad que exista
-    */
-
    cout << "Proyecto 1 de Vicente Farias Pina" << endl;
-   cout << "Este programa tratara de unos guardianes y maestros " << endl;
-   cout << "" << endl;
+   cout << "Este programa tratara de los guardianes de un reino donde lucharan entre varias ciudades para determinar " << endl;
+   cout << "quien es el nuevo guardian del reino, para esto tendras que elegir un candidato que te permita pelear con otros guardianes recorriendo ciudades" << endl;
+   cout << "y obteniendo puntos" << endl;
+
    //Lectura Ciudades
-   cout << "Lectura del archivo ciudades" << endl;
-   int contadorLine = 0;
-   string nameFile = "pruebatxt.txt";
-   ifstream file(nameFile.c_str());
-   string lineaCiudad;
+   //formato [city, connectedCity]
+    cout << "Lectura del archivo ciudades" << endl;
+    vector<CIUDADES> ciudades = leerCiudades("ciudadestxt.txt");
    
-    // Obtener línea de archivo, y almacenar contenido en "linea"
-    while (getline(file, lineaCiudad)) {
-        // Imprimir el contenido
-        cout << "Linea numero " << contadorLine << ": ";
-        cout << lineaCiudad << endl;
-        contadorLine++;
-    }
-    int sizeLine =  lineaCiudad.size();
-    cout <<sizeLine << endl;
+   //Text formato de ejemplo  nombre,poder,maestro,ciudad       Freya,100,None,Tesla Capital
+   // Lectura Guardianes
+    ifstream guardianFile("guardians.txt");
+    vector<GUARDIANES> guardianes = leerGuardianes("guardians.txt");
 
-
-    //Lectura Guardianes
-    cout << "Lectura del archivo guardianes" << endl;
-    contadorLine = 0;
-    string guardianFile = "guardians.txt";
-    ifstream guardian(guardianFile.c_str());
-    string lineaGuardian;
-
-    while (getline(guardian, lineaGuardian))
+    int opMenu = 0;
+    do
     {
-        cout << "Linea numero " << contadorLine << ": ";
-        cout << lineaGuardian << endl;
-        contadorLine++;
-    }
-    sizeLine = lineaGuardian.size();
-    cout << sizeLine << endl;
-    
+        //menu
+        cout << "\n\n******MENU*******"<< endl;
+        cout << "       Opcion1: Ver la carga del texto de guardianes (sus datos), ver el texto ciudades y sus conexiones "<< endl;
+        cout << "       Opcion2: Ver las reglas nuevamente \n       Opcion3: Ver la lista de candidatos. \n      Opcion4: Ver al guardian. \n      Opcion5: Conocer el reino." << endl;
+        cout << "      Opcion6: Presenciar una Batalla. \n      Opcion0: Salir del programa" << endl;
+        cin >> opMenu;
+        switch (opMenu)
+        {
+        case 1: //carga de informacion y muestra de los guardianes
+            cout << "Elegiste ver los guardianes cargados" << endl;
+            mostrarGuardianes(guardianes);
+        
+            break;
+        
+        case 2: reglas();
+            break;
+        
+        case 3: cout << "Elegiste ver la lista de candidatos" << endl;
+            break;
+        
+        case 4: cout <<"Ver al guardian" <<endl;
+            break;
+        case 5: cout << "Conocer el reino" << endl;
+            break;
+
+        case 6: cout << "Presenciar una Batalla" << endl;
+            break;
+        default:
+            break;
+        }
+    } while (opMenu != 0);
     
 
-   
-   cout <<"                    ***FIN DEL PROGRAMA***" << endl;
+    cout <<"                    ***FIN DEL PROGRAMA***" << endl;
 
     return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Proyecto2-VicenteFariasPina
- Este es el repositorio para el proyecto2 del modulo de algoritmos del año 2023
+ Este es el repositorio para el proyecto2 del modulo de algoritmos del año 2023 de Vicente Farías Piña
+
+ Explicacion del funcionamiento del proyecto.
+
+
+ Explicacion texto guardianes "Palidar que hay al menos un guardián con poder 100 y entre 1 y 3 guardianes con poder entre 90 y 99"
+   /*Si no hay un guardian con 100 o mas de uno con 100 no funciona
+    Maximo de guardianes de la carga inicial es de 3 entre 90 y 99
+    Maestro del reino 100
+    guardianes del reino 90-99
+    candidatos a guardianes del reino < 90
+    */
